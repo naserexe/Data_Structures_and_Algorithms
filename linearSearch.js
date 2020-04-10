aa5ac312fd2aebed14a112a3f89ddb8f53cbe237
Naser <nasersoftcorporation2013@gmail.com>
2020-04-10 07:41:15 +0600

    Eslint configured

--- linearSearch.js
+++ linearSearch.js
@@ -1,10 +1,10 @@
 function linearSearch(arr, value) {
-  for(let i = 0; i < arr.length; i++){
-    if(arr[i] === value){
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === value) {
       return i;
     }
   }
-  return -1
+  return -1;
 }
 
-console.log(linearSearch([12,25,36,1,2,4,56,47,], 100));
+console.log(linearSearch([10, 14, 23, 1, 25, 31, 2, 3, 65], 23));
