421133a797f8841a237e77db42264b00e98ae4ca
Naser <nasersoftcorporation2013@gmail.com>
2020-04-10 20:46:39 +0600

    Binary search

---
+++ binarySearch.js
@@ -0,0 +1,14 @@
+function binarySearch(arr, value) {
+  let start = 0;
+  let end = arr.length - 1;
+  let middle = Math.floor((start + end) / 2);
+
+  while (arr[middle] !== value && start <= end) {
+    if (value < arr[middle]) end = middle - 1;
+    else start = middle + 1;
+    middle = Math.floor((start + end) / 2);
+  }
+  return arr[middle] === value ? middle : -1;
+}
+
+console.log(binarySearch([3, 5, 6, 7, 9, 12], 12));
